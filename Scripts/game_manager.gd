178a69a45af6fc5082a27c65e502a6b2b0364253
Intedai <arielmderi@gmail.com>
2024-05-05 00:02:11 +0300

    Canvas and coins on screen

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,8 +2,9 @@ extends Node
 
 # Coins that the player earned from cutting trees
 var coins = 0
+@onready var score_label = %ScoreLabel
 
 func add_coin():
 	coins += 1
 	# Placeholder for when coins will be shown in the UI
-	print(coins)
+	score_label.text = str(coins)
